4c32e8aff8a3f65fe1f5f920ed1c90fe530e07cf
unknown <Mdshowanhossain11@gmail.com>
2021-08-31 21:21:32 +0600

    Registration_LogIn_Rest_Api

--- src/db/connection.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/employee')
-    .then(() => console.log('Your Connection is established'))
-    .catch((err) => console.log(err))
